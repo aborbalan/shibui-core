67ced74aee5b55d0c98254ddb6e3e42dd4ed72f4
Alejandro Borbalán <aborbalan.duenas@gmail.com>
2026-03-13 19:23:10 +0100

    feat(layout): add lib-bento-grid + lib-bento-item 1

--- packages/shibui-ui/src/components/atoms/bento-item/lib-bento-item.html.ts
+++ packages/shibui-ui/src/components/atoms/bento-item/lib-bento-item.html.ts
@@ -1,16 +1,12 @@
 import { html, TemplateResult } from 'lit';
-import { LibBentoItem } from './lib-bento-item.component';
+import type { LibBentoItem } from './lib-bento-item.component';
 
-export const bentoItemTemplate = (context: LibBentoItem): TemplateResult => html`
-  <div 
-    class="bento-item"
-    style="
-      --lib-bento-item-cols: ${context.cols};
-      --lib-bento-item-rows: ${context.rows};
-    "
-  >
-    <div class="content">
-      <slot></slot>
+export function bentoItemTemplate(_ctx: LibBentoItem): TemplateResult {
+  return html`
+    <div class="bento-item" part="item">
+      <div class="content" part="content">
+        <slot></slot>
+      </div>
     </div>
-  </div>
-`;
+  `;
+}
